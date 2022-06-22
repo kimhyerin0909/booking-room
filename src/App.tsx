e376f2bcdb8f1a9676225c12d2ff880a28133699
kimhyerin0909 <khyerin0909@gmail.com>
2022-06-22 22:23:40 +0900

    Feat - link elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      Booking room
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
